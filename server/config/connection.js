917f8866f35d8a8228f443f43c9c7c332a93f90b
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2023-03-02 21:35:01 -0700

    connected mongoose/MongoDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/django', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/workout_blog', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
