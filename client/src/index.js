28152d80cc8e9e263ca185683aa000fde246ceb3
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2023-03-08 14:35:27 -0700

    adding service worker

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,23 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import { Workbox } from 'workbox-window';
+// import '../style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
+
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
+  workboxSW.register();
+} else {
+  console.error('Service workers are not supported in this browser.');
+}
+
 
