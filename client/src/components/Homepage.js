87b2d3c84704ab298c5e6a7d1dbdac0c16ce3b7d
Shannon <shannonpuno@arizona.edu>
2023-03-09 14:09:47 -0700

    added login function

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import User from './User';
-import Blog from './Blog';
+import React, { useState, useEffect } from 'react';
+import { Card } from 'react-bootstrap';
+
+import { useQuery} from '@apollo/client';
+import { GET_USER } from '../utils/queries';
+
+
+
+const User = ({ user: { username }}) => (
+    <Card>
+        <Card.Body>
+            <Card.Title>{username}</Card.Title>
+        </Card.Body>
+    </Card>
+)
 
 function Homepage() {
-    return (
-        <div class="container text-center">
-            <div class="row">
-                <div class="col">
-                    < User />
-                 </div>
-                <div class="col-6">
-                    < Blog />
-                </div>
-            </div>
-        </div>
 
+    const { loading, error, data } = useQuery(GET_USER);
+
+    if (error) return <h1>Something Went Wrong!</h1>
+    if (loading) return <h1>loading...</h1>
+
+    return (
+        <main className='App'>
+            <h1>Welcome!</h1>
+            {data.users.map(user => (
+                <User key={user.id} user={user} />
+            ))}
+        </main>
     );
 };
 
